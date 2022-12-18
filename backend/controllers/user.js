de9469ca8d79366fa011b83424ee2cfcaaf8be5c
Jade Jones <jadejonescrypto@gmail.com>
2022-12-18 01:01:34 +0000

    log in and sign up creating errors

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,26 +6,18 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10).then(
         (hash) => {
             const user = new User({email: req.body.email,
-            password: hash
-        });
+            password: hash,
+          });
         user.save().then(
             () => {
                 res.status(201).json({
                     message: 'User added successfully'
                 })
-            }
-        )
+            })
         .catch((error) => res.status(400).json({ error }))
-        .catch(
-            (error) =>{
-                res.status(500).json({
-                 error
-                });
-            }
-        )
-        }
-    )
-};
+        .catch((error) =>res.status(500).json({ error }))
+        })
+      };
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email }).then(
--- tasklist.md
+++ tasklist.md
@@ -2,17 +2,17 @@
 <!-- * set up necessary files -->
 <!-- * install express -->
 <!-- * use express.json() to parse the request -->
-* install mongoose
-* connect to mongoDB and allow all IP addresses to connect to the cluster
+<!-- * install mongoose -->
+<!-- * connect to mongoDB and allow all IP addresses to connect to the cluster -->
 
-* user model
-* user route
-* user controller
+<!-- * user model -->
+<!-- * user route -->
+<!-- * user controller -->
 
-* check can create an account
+<!-- * check can create an account -->
 * check login and valid token
 
-* make sure password is hashed 
+<!-- * make sure password is hashed  -->
 * check user cannot sigfn up more than once with the same email
 
 Do:
