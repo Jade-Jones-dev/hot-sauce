4716118edfa2bd5612297888bd57a7a35479865c
Jade Jones <jadejonescrypto@gmail.com>
2022-12-26 21:11:44 +0000

    get route and controller

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -15,19 +15,19 @@ exports.getAllSauces = (req, res, next) => {
 };
 
 // getOneSauce
-// exports.getOneSauce = (req, res, next) => {
-	// Sauce.findOne({
-		// _id: req.params.id,
-	// })
-		// .then((sauce) => {
-			// res.status(200).json(sauce);
-		// })
-		// .catch((error) => {
-			// res.status(404).json({
-				// error: error,
-			// });
-		// });
-// };
+exports.getOneSauce = (req, res, next) => {
+	Sauce.findOne({
+		_id: req.params.id,
+	})
+		.then((sauce) => {
+			res.status(200).json(sauce);
+		})
+		.catch((error) => {
+			res.status(404).json({
+				error: error,
+			});
+		});
+};
 // 
 // router.put('/:id', auth, sauceCtrl.modifySauce);
 // router.delete('/:id', auth, sauceCtrl.deleteSauce);
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,8 +8,8 @@ const multer = require('../middleware/multer-config')
 
 
 router.get("/", auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 // router.post('/', auth, sauceCtrl.createSauce);
-// router.get('/:id', auth, sauceCtrl.getOneSauce);
 // router.put('/:id', auth, sauceCtrl.modifySauce);
 // router.delete('/:id', auth, sauceCtrl.deleteSauce);
 // like a sauce
