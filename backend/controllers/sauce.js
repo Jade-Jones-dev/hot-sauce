c87b7636dbad82111f10f81c11186d623af2a089
Jade Jones <jadejonescrypto@gmail.com>
2022-12-30 17:03:44 +0000

    modify working

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -78,8 +78,48 @@ exports.deleteSauce = (req, res, next) => {
 		});
 	});
 };
+
 // router.put('/:id', auth, sauceCtrl.modifySauce);
-// router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
+exports.modifySauce = (req, res, next) => {
+	let sauce = new Sauce({ _id: req.params._id });
+	if (req.file) {
+		const url = req.protocol + "://" + req.get("host");
+		req.body.thing = JSON.parse(req.body.thing);
+		sauce = {
+			_id: req.params.id,
+			name: req.body.sauce.name,
+         manufacturer: req.body.sauce.manufacturer,
+			description: req.body.sauce.description,
+			mainPepper: req.body.sauce.mainPepper,
+			imageUrl: url + "/images/" + req.file.filename,
+			heat: req.body.sauce.heat,
+			userId: req.body.sauce.userId,
+		};
+	} else {
+		sauce = {
+         _id: req.params.id,
+			name: req.body.name,
+         manufacturer: req.body.manufacturer,
+			description: req.body.description,
+			mainPepper: req.body.mainPepper,
+			imageUrl: req.body.imageUrl,
+			heat: req.body.heat,
+			userId: req.body.userId,
+		};
+	}
+	Sauce.updateOne({ _id: req.params.id }, sauce)
+		.then(() => {
+			res.status(201).json({
+				message: "Thing updated successfully!",
+			});
+		})
+		.catch((error) => {
+			res.status(400).json({
+				error: error,
+			});
+		});
+};
 
 // like a sauce
 // dislike a sauce
