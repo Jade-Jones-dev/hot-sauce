56dac873accf6108fa605705e6f7c0077f23e2dd
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-01-17 07:31:19 +0000

    rf

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const path = require("path");
 var dotenv = require('dotenv');
 dotenv.config();
+const rateLimit = require('express-rate-limit')
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
@@ -37,12 +38,19 @@ app.use((req, res, next) => {
     next();
   });
 
+  const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+
 app.use(express.json());
 
 app.use("/images", express.static(path.join(__dirname, "images"))); 
 app.use('/api/auth', userRoutes);
 
-app.use('/api/sauces', sauceRoutes);
+app.use('/api/sauces', apiLimiter, sauceRoutes);
 
 module.exports = app;
 
